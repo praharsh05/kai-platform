a1e65bab3924b9fdfe480046017371f21dae1c73
praharsh05 <praharsh.dubey05@gmail.com>
2024-09-03 14:25:21 +0100

    addition of auth

--- frontend/firebase/config.js
+++ frontend/firebase/config.js
@@ -1,4 +1,7 @@
 // paste your firebase config object here
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_AUTH_DOMAIN,
@@ -7,6 +10,11 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
 
 // stop editing below this
 export default firebaseConfig;
